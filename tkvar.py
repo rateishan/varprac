e32078a6fb3957775a8c21a6b6287469e05166d7
rateishan <ishanrate@gmail.com>
2024-05-27 01:10:37 +0530

    add entry and lable

--- tkvar.py
+++ tkvar.py
@@ -1,10 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
+def button_click():
+    print("button clicked")
 
 root  =tk.Tk()
 root.title('Buttons')
 root.geometry('300x200')
 root.iconbitmap('mars.ico')
+
+def button_fun(n1,n2):
+    sum= n1+n2
+    answer.set(f'answer is {sum}')
+
+
+
+num1=tk.IntVar()
+num2=tk.IntVar()
+answer =tk.StringVar()
+
+entry1=tk.Entry(root , textvariable=num1)
+entry1.pack()
+
+entry2=tk.Entry(root , textvariable= num2)
+entry2.pack()
+
+
+button=ttk.Button(root, text="Click", command=lambda:button_fun(num1.get(), num2.get()))
+button.pack()
+button.place(x=100,y=50)
+
+
+label=ttk.Label(root, textvariable=answer)
+label.pack()
+label.place(x=10,y=10)
+
 
 
 
