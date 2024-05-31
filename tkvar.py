e39d715a4f175a5fbbd8a656ad45b3cc5938f121
rateishan <ishanrate@gmail.com>
2024-06-01 00:47:27 +0530

    button place

--- tkvar.py
+++ tkvar.py
@@ -8,8 +8,8 @@ root.title('Buttons')
 root.geometry('300x200')
 root.iconbitmap('mars.ico')
 
-def button_fun(n1,n3):
-    sum= n1+n3
+def button_fun(n1,n2):
+    sum= n1+n2
     answer.set(f'answer is {sum}')
 
 
@@ -20,19 +20,22 @@ answer =tk.StringVar()
 
 entry1=tk.Entry(root , textvariable=num1)
 entry1.pack()
+entry1.place(x=90,y=25)
+
 
 entry2=tk.Entry(root , textvariable= num2)
 entry2.pack()
+entry2.place(x=90,y=50)
 
 
 button=ttk.Button(root, text="Click", command=lambda:button_fun(num1.get(), num2.get()))
 button.pack()
-button.place(x=100,y=50)
+button.place(x=110,y=150)
 
 
 label=ttk.Label(root, textvariable=answer)
 label.pack()
-label.place(x=10,y=10)
+label.place(x=110,y=100)
 
 
 
