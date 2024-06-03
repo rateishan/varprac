e3dcf74d72d023a299adee51c5e57e9f7484b166
rateishan <ishanrate@gmail.com>
2024-06-04 00:53:57 +0530

    adjest the space of root

--- tkvar.py
+++ tkvar.py
@@ -5,8 +5,13 @@ def button_click():
 
 root  =tk.Tk()
 root.title('Buttons')
-root.geometry('300x200')
+root.geometry('800x400')
 root.iconbitmap('mars.ico')
+root.resizable(True,True)
+
+table=ttk.Treeview(root)
+table.pack()
+table.place(x=150,y=50)
 
 def button_fun(n1,n2):
     sum= n1+n2
@@ -20,22 +25,22 @@ answer =tk.StringVar()
 
 entry1=tk.Entry(root , textvariable=num1)
 entry1.pack()
-entry1.place(x=90,y=25)
+entry1.place(x=10,y=10)
 
 
 entry2=tk.Entry(root , textvariable= num2)
 entry2.pack()
-entry2.place(x=90,y=50)
+entry2.place(x=10,y=30)
 
 
 button=ttk.Button(root, text="Click", command=lambda:button_fun(num1.get(), num2.get()))
 button.pack()
-button.place(x=110,y=150)
+button.place(x=10,y=70)
 
 
 label=ttk.Label(root, textvariable=answer)
 label.pack()
-label.place(x=110,y=100)
+label.place(x=10,y=100)
 
 
 
